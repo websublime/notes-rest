15eadcf791d098acddce839b66dbb4024c089f34
Miguel Ramos <miguel.marques.ramos@gmail.com>
2014-05-15 01:41:11 +0100

    Created AnnotationHashMatcher class.

---
+++ src/League/Notes/Annotation/AnnotationHashMatcher.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of NotesRest Project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * PHP version 5
+ *
+ * @category  Annotation
+ * @package   League\Notes\Annotation
+ * @author    Miguel Ramos <miguel.marques.ramos@gmail.com>
+ * @copyright 2012-2014 Websublime.com
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @release   GIT: $Id: v0.0.1
+ * @link      http://symphonic.websublime.com
+ */
+
+namespace League\Notes\Annotation;
+
+use League\Notes\Parser\ParallelMatcher;
+
+/**
+ * Description
+ *
+ * @category  Annotation
+ * @package   League\Notes\Annotation
+ * @author    Miguel Ramos <miguel.marques.ramos@gmail.com>
+ * @copyright 2012-2014 Websublime.com
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   Release: v0.0.1
+ * @link      http://symphonic.websublime.com
+ */
+class AnnotationHashMatcher extends ParallelMatcher
+{
+    protected function build()
+    {
+        $this->add(new AnnotationPairMatcher);
+        $this->add(new AnnotationMorePairsMatcher);
+    }
+}
+/** @end AnnotationHashMatcher.php */
+ 
